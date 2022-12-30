06c76bfaf57d253a8c951f67dd342098264e08e9
Brandon Lam <brandonlam4237@gmail.com>
2022-12-30 13:19:20 -0500

    init project files

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./assets/scss/app.scss";
+import { useState } from "react";
+
+function App() {
+  const [page, setPage] = useState("home");
+  return (
+    <div className="App">
+      <div className="nav">
+        <div className="menu">
+          <input
+            type="radio"
+            id="radio-1"
+            name="menu"
+            value="home"
+            onChange={(e) => setPage(e.target.value)}
+            defaultChecked
+          />
+          <label class="opt" for="radio-1">
+            Home
+          </label>
+
+          <input
+            type="radio"
+            id="radio-2"
+            name="menu"
+            value="projects"
+            onChange={(e) => setPage(e.target.value)}
+          />
+          <label class="opt" for="radio-2">
+            Projects
+          </label>
+
+          <input
+            type="radio"
+            id="radio-3"
+            name="menu"
+            value="contact"
+            onChange={(e) => setPage(e.target.value)}
+          />
+          <label class="opt" for="radio-3">
+            Contact
+          </label>
+
+          <span className="glider"></span>
+        </div>
+      </div>
+      <div className="pages">
+        {page === "home" && <div>Home</div>}
+        {page === "projects" && <div>Projects</div>}
+        {page === "contact" && <div>Contact</div>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
